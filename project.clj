ff0639c4d1a6ccd6e27c0aa10fdd37d7f23c7a59
JohnnyJayJay <johnnyjayjay02@gmail.com>
2021-06-21 11:43:40 +0200

    Bump minor version to 0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.github.johnnyjayjay/ring-discord-auth "0.1.0"
+(defproject com.github.johnnyjayjay/ring-discord-auth "0.2.0"
   :description "Fast and secure ring middleware to verify ED-25519-signed Discord interactions"
   :url "https://github.com/JohnnyJayJay/ring-discord-auth"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
